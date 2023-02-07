bbb29753fc355dfb82c14dd95e424ab159a0e1e5
양지현 <eve@eve-MacBook-Pro.local>
2023-02-07 21:13:54 +0900

    write submit

--- src/main/java/com/study/board/repository/BoardRepository.java
+++ src/main/java/com/study/board/repository/BoardRepository.java
@@ -1,13 +1,10 @@
 package com.study.board.repository;
 
-
-import com.study.board.entity.Board;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.study.board.entity.Board;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Integer> {
-}
 
-//기본키로 지정한 항목의 타입
+}
